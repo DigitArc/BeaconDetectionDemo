3775636956986faedc0902f5bb1aa922da1ae6d6
emrekara37 <emre.kara5@ogr.sakarya.edu.tr>
2020-10-05 14:14:36 +0300

    Latest

--- BeaconDetectionDemo/MainActivity.cs
+++ BeaconDetectionDemo/MainActivity.cs
@@ -1,10 +1,17 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Collections.Generic;
+using System.Net;
 using Android.OS;
 using Android.App;
+using Android.Graphics;
 using Android.Widget;
 using Android.Support.V7.App;
+using Java.Net;
+using Java.Util;
 using Org.Altbeacon.Beacon;
+using Region = Org.Altbeacon.Beacon.Region;
+using Uri = Android.Net.Uri;
 
 
 namespace BeaconDetectionDemo
@@ -14,7 +21,22 @@ namespace BeaconDetectionDemo
     {
         private TextView tvDistance;
         private BeaconManager beaconManager;
+        private ImageView image;
+        private Bitmap GetImageBitmapFromUrl(string url)
+        {
+            Bitmap imageBitmap = null;
 
+            using (var webClient = new WebClient())
+            {
+                var imageBytes = webClient.DownloadData(url);
+                if (imageBytes != null && imageBytes.Length > 0)
+                {
+                    imageBitmap = BitmapFactory.DecodeByteArray(imageBytes, 0, imageBytes.Length);
+                }
+            }
+
+            return imageBitmap;
+        }
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -22,7 +44,9 @@ namespace BeaconDetectionDemo
             SetContentView(Resource.Layout.activity_main);
 
             tvDistance = FindViewById<TextView>(Resource.Id.tvDistance);
-
+            image = FindViewById<ImageView>(Resource.Id.digitarc);
+            var imageBitmap = GetImageBitmapFromUrl("https://digitarc.net/assets/img/digitarc-logo-gri.png");
+            image.SetImageBitmap(imageBitmap);
             beaconManager = BeaconManager.GetInstanceForApplication(this);
 
             //-------------------------
@@ -33,6 +57,14 @@ namespace BeaconDetectionDemo
             //beaconManager.BeaconParsers.Add(beaconParser);
             //-------------------------
 
+            var beaconParser = new BeaconParser();
+            beaconParser.SetBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24");
+
+            var beaconParser2 = new BeaconParser();
+            beaconParser2.SetBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
+
+            beaconManager.BeaconParsers.Add(beaconParser);
+            beaconManager.BeaconParsers.Add(beaconParser2);
             beaconManager.AddRangeNotifier(this);
             beaconManager.Bind(this);
         }
@@ -47,15 +79,40 @@ namespace BeaconDetectionDemo
 
         public void OnBeaconServiceConnect()
         {
-            beaconManager.StartRangingBeaconsInRegion(new Region("myRangingRegion", null, null, null));
+            try
+            {
+                beaconManager.StartRangingBeaconsInRegion(new Region("2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6",
+                    Identifier.FromUuid(UUID.FromString("2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6")),
+                    Identifier.FromInt(1),
+                    Identifier.FromInt(1)));
+                //beaconManager.StartRangingBeaconsInRegion(new Region(Guid.NewGuid().ToString(), new List<Identifier>
+                //{
+                //    Identifier.FromUuid(UUID.FromString("0000ffe0-0000-1000-8000-00805f9b34fb")),
+                //    Identifier.FromUuid(UUID.FromString("5276d744-90c9-40a7-960e-cc1c3764bc40")),
+                //    Identifier.FromUuid(UUID.FromString("dd2c373f-ea1b-4b7f-bee0-e07110d2ae06"))
+                //}));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
+        private bool isShowToastMessage = false;
         public void DidRangeBeaconsInRegion(ICollection<Beacon> p0, Region p1)
         {
             if (!p0.Any())
                 return;
+            var toast = Toast.MakeText(Application.Context, "Beacon bulundu", ToastLength.Short);
+            if (isShowToastMessage == false)
+            {
+                toast.Show();
+                isShowToastMessage = true;
+            }
 
-            RunOnUiThread(() => {
+            RunOnUiThread(() =>
+            {
                 tvDistance.Text = p0.First().Distance.ToString("0.00");
             });
         }
